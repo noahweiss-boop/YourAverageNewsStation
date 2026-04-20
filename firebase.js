5ef81e49b9c6c005af78dfc2a27362c0ec9bea4f
SimplyAverage <noahweiss@odysseyk12.org>
2026-04-19 20:12:37 -0700

    Create firebase.js
    Init. Firebase Auth

---
+++ firebase.js
@@ -0,0 +1,12 @@
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQFzHILDhefvRnrIPSF2OzhIFPOe42VVY",
+    authDomain: "yans-auth.firebaseapp.com",
+    projectId: "yans-auth",
+    storageBucket: "yans-auth.firebasestorage.app",
+    messagingSenderId: "538236782184",
+    appId: "1:538236782184:web:6a28ee8a9ce40425d25ddd"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+</script>
