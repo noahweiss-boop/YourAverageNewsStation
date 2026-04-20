a31c8f841dcd2048593c26ee0c21861d4f39d9c8
SimplyAverage <noahweiss@odysseyk12.org>
2026-04-19 20:25:08 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAQFzHILDhefvRnrIPSF2OzhIFPOe42VVY",
-    authDomain: "yans-auth.firebaseapp.com",
-    projectId: "yans-auth",
-    storageBucket: "yans-auth.firebasestorage.app",
-    messagingSenderId: "538236782184",
-    appId: "1:538236782184:web:6a28ee8a9ce40425d25ddd"
-  };
+  apiKey: "AIzaSyAQFzHILDhefvRnrIPSF2OzhIFPOe42VVY",
+  authDomain: "yans-auth.firebaseapp.com",
+  projectId: "yans-auth",
+  storageBucket: "yans-auth.firebasestorage.app",
+  messagingSenderId: "538236782184",
+  appId: "1:538236782184:web:6a28ee8a9ce40425d25ddd"
+};
 
 firebase.initializeApp(firebaseConfig);
